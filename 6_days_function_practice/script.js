4137337d3da5066865fd669a0a8772990b81c235
Arif <60351443+protesq@users.noreply.github.com>
2025-01-30 22:36:42 +0300

    func_pratice

---
+++ 6_days_function_practice/script.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+document.getElementById('userForm').addEventListener('submit', function (event) {
+  event.preventDefault(); // Formun varsayılan gönderme işlemini durdur
+
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const age = document.getElementById('age').value;
+  alert('Form başarıyla gönderildi!');
+  console.log(name, email, age);
+});
